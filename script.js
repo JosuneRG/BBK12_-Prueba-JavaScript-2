208a669dadeb2b548e4c1e64b90e5d5d451d08ad
JosuneRG <josunerg94@gmail.com>
2025-03-31 18:36:21 +0200

    cambios realizados

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // Ejercicio 1:
 // Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase
-function contarLetra(frase, letra) {
+function contarLetra(frase, letra) 
+{
     let contador = 0;
-    for (let i = 0; i < frase.length; i++) {
-        if (frase[i].toLowerCase() === letra.toLowerCase()) {
+    for (let i = 0; i < frase.length; i++) 
+    {
+        if (frase[i].toLowerCase() === letra.toLowerCase()) 
+        {
             contador++;
         }
     }
